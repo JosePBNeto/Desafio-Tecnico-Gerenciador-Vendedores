355b8df26fad9569bc57103b56c0d8a476d47621
Jose Patricio Neto <josepatricioneto@hotmail.com>
2024-05-17 01:33:08 -0300

    feature: Endpoint simples para executar as funções calcular_comissoes e calcular_volume_e_media_vendas

--- Rotas.py
+++ Rotas.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify, make_response
 from Vendedor import GerenciarVendedor, Vendedor
-from Excel_functions import criar_atualizar_em_lotes
+from Excel_functions import criar_atualizar_em_lotes, calcular_comissoes, calcular_volume_e_media_vendas
 
 app = Flask(__name__)
 
 gerenciadorVendedor = GerenciarVendedor()
+FILE_PATH_VENDAS = 'Vendas.xlsx'
+FILE_PATH_VENDEDORES = "planilha_vendedores.xlsx"
 
 @app.route('/vendedores', methods=['POST'])
 def create_vendedor():
@@ -34,7 +36,6 @@ def get_vendedor(cpf):
 
 @app.route('/vendedores/<cpf>', methods=['PUT'])
 def update_vendedor(cpf):
-
     vendedor_existente = gerenciadorVendedor.read_vendedor(cpf)
 
     if vendedor_existente:
@@ -70,11 +71,11 @@ def delete_vendedor(cpf):
 @app.route('/vendedores', methods=['GET'])
 def get_all_vendedores():
     vendedores = gerenciadorVendedor.read_all_vendedores()
-    return jsonify([{"cpf": v[0], "nome": v[1], "data_nascimento": v[2], "email": v[3], "estado": v[4]} for v in vendedores])
+    return jsonify([{"cpf": v[0], "nome": v[1], "data_nascimento": v[2], "email": v[3], "estado": v[4]} for v in vendedores]), 200
 
 @app.route('/vendedores/planilha', methods=['POST'])
 def update_planilha_vendedor():
-    lista_de_vendedores = criar_atualizar_em_lotes()
+    lista_de_vendedores = criar_atualizar_em_lotes(FILE_PATH_VENDEDORES)
 
     vendedores_json = []
     for vendedor in lista_de_vendedores:
@@ -87,10 +88,19 @@ def update_planilha_vendedor():
         }
         vendedores_json.append(vendedor_dict)
 
-    # Retornando a lista de vendedores em formato JSON
-    return jsonify({"message": "Vendedores atualizados ou adicionados", "vendedores": vendedores_json})
+    return jsonify({"message": "Vendedores atualizados ou adicionados", "vendedores": vendedores_json}), 201
 
+@app.route('/vendedores/calcularComissao', methods=['POST'])
+def calcular_planilha_vendedor():
+    calcular_comissoes(FILE_PATH_VENDAS)
 
+    return jsonify({'success': 'Comissões calculadas sucesso'}), 201
+
+@app.route('/vendedores/calcularVolumeVendasPorCanal', methods=['POST'])
+def calcular_volume_media_por_vendedor():
+    calcular_volume_e_media_vendas(FILE_PATH_VENDAS)
+
+    return jsonify({'success': 'Volume de vendas e média por profissional por canal calculadao com sucesso'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
